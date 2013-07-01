0b5638d4cd9cf5eef4e8009370ecefc96378dcd8
nemo <git@nemo.ikkoku.de>
2013-07-01 02:40:48 +0200

    Use session to store visits

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ import (
 	"net/http"
 	"github.com/PuerkitoBio/goquery"
 	"code.google.com/p/go.net/html"
+	"github.com/gorilla/sessions"
 )
+
+var encryptionKey = "lirumlarum"
+var store = sessions.NewCookieStore([]byte(encryptionKey))
 
 func setAttributeValue(n *html.Node, attrName, value string) error {
 	for i, a := range n.Attr {
@@ -61,6 +65,20 @@ func errorHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func panicOnError(err error) {
+	panic(err)
+}
+
+func serveWikiPage(host, page string, w http.ResponseWriter) {
+	content, err := rewriteWikiUrls("http://" + host + "/wiki/" + page)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, content)
+}
+
 // Accepts visits and serves new wiki page
 func visitHandler(w http.ResponseWriter, r *http.Request) {
 	defer errorHandler(w, r)
@@ -71,18 +89,39 @@ func visitHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	content, err := rewriteWikiUrls("http://de.wikipedia.org/wiki/" + values.Get("page"))
+	host := values.Get("host")
+	page := values.Get("page")
 
-	if err != nil {
-		panic(err)
+    session, _ := store.Get(r, "game")
+
+	if i, ok := session.Values["visits"].(int); !ok {
+		session.Values["visits"] = 1
+	} else {
+		session.Values["visits"] = i + 1
 	}
 
-	fmt.Fprintf(w, content)
+	session.Values["opponent"] = "someName"
+
+    session.Save(r, w)
+
+	serveWikiPage(host, page, w)
 }
+
+func visitsHandler(w http.ResponseWriter, r *http.Request) {
+	defer errorHandler(w, r)
+
+    session, _ := store.Get(r, "game")
+
+	fmt.Fprintf(w, "visits: %d, opponent: %s\n", session.Values["visits"],
+		session.Values["opponent"])
+}
+
 
 // Serves initial page
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	wikiUrl := "/visit?page=jQuery"
+	defer errorHandler(w, r)
+
+	wikiUrl := "/visit?page=jQuery&host=de.wikipedia.org"
 	doc := `
 <html>
 	<iframe src="%s"></iframe>
@@ -92,11 +131,15 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, doc, wikiUrl, html.EscapeString(r.URL.Path))
 }
 
+// TODO: send new visit and visit of opponent to website
+
 func main() {
 //	serveWikiUrl("http://de.wikipedia.org/wiki/jQuery")
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/visit", visitHandler)
+	http.HandleFunc("/visits", visitsHandler)
+	http.HandleFunc("/serve", visitHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
