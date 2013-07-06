59fec7f16da1c646a9e580488049adcef4ba901d
nemo <git@nemo.ikkoku.de>
2013-07-06 21:05:50 +0200

    Handle case where there is no winner

--- main.go
+++ main.go
@@ -75,9 +75,11 @@ func visitHandler(w http.ResponseWriter, r *http.Request) {
 		// He reached the goal
 
 		// This one is the winner (for now)
-		if len(game.WinnerPath) > len(session.Visits()) {
+		if len(game.WinnerPath) == 0 || len(game.WinnerPath) > len(session.Visits()) {
 			game.Winner = session.PlayerName()
 			game.WinnerPath = session.Visits()
+
+			// TODO: announce new winner
 		}
 
 		templates.ExecuteTemplate(w, "win.html", struct{
