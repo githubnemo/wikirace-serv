aa44fd62abb65f7fb8437e3072711d0ffff8b583
nemo <git@nemo.ikkoku.de>
2013-07-07 21:16:25 +0200

    Format JS

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
-	var sock = new WebSocket("ws://91.97.71.252:8080/client");
-	console.log("Websocket - status: " + sock.readyState);
-	sock.onopen = function(m) {
-	  console.log("CONNECTION opened..." + this.readyState);}
-	sock.onmessage = function(m) {
-	  console.log(m.data);
-	  //var msg = jQuery.parseJSON(m.data);
-
-	  //$('#log').append('<li>' + msg + ' clicked on ' + msg.Message + '</li>');
+	function getServerAddress() {
+		return location.host;
 	}
+
+	var sock = new WebSocket("ws://"+getServerAddress()+"/client");
+
+	console.log("Websocket - status: " + sock.readyState);
+
+	sock.onopen = function(m) {
+		console.log("CONNECTION opened..." + this.readyState);
+	}
+
+	sock.onmessage = function(m) {
+		console.log(m.data);
+		//var msg = jQuery.parseJSON(m.data);
+
+		//$('#log').append('<li>' + msg + ' clicked on ' + msg.Message + '</li>');
+	}
+
 	sock.onerror = function(m) {
-	  console.log("Error occured sending..." + m.data);}
+		console.log("Error occured sending..." + m.data);
+	}
+
 	sock.onclose = function(m) {
-	  console.log("Disconnected - status " + this.readyState);}
+		console.log("Disconnected - status " + this.readyState);
+	}
 });
