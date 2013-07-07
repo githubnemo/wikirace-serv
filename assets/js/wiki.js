0ba9a8a2c3c30caad26f6de7cfdd46623c5c4f2a
nemo <git@nemo.ikkoku.de>
2013-07-07 21:13:22 +0200

    Errors are printed on the console anyway

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
-  try {
+	var sock = new WebSocket("ws://91.97.71.252:8080/client");
+	console.log("Websocket - status: " + sock.readyState);
+	sock.onopen = function(m) {
+	  console.log("CONNECTION opened..." + this.readyState);}
+	sock.onmessage = function(m) {
+	  console.log(m.data);
+	  //var msg = jQuery.parseJSON(m.data);
 
-    var sock = new WebSocket("ws://91.97.71.252:8080/client");
-    console.log("Websocket - status: " + sock.readyState);
-    sock.onopen = function(m) { 
-      console.log("CONNECTION opened..." + this.readyState);}
-    sock.onmessage = function(m) { 
-      console.log(m.data);
-      //var msg = jQuery.parseJSON(m.data);
-
-      //$('#log').append('<li>' + msg + ' clicked on ' + msg.Message + '</li>');
-    }
-    sock.onerror = function(m) {
-      console.log("Error occured sending..." + m.data);}
-    sock.onclose = function(m) { 
-      console.log("Disconnected - status " + this.readyState);}
-  } catch(exception) {
-    console.log(exception);
-  }
+	  //$('#log').append('<li>' + msg + ' clicked on ' + msg.Message + '</li>');
+	}
+	sock.onerror = function(m) {
+	  console.log("Error occured sending..." + m.data);}
+	sock.onclose = function(m) {
+	  console.log("Disconnected - status " + this.readyState);}
 });
