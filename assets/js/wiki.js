41683a3b1283bacbea95bd99a02bfcf1f41a408e
nemo <git@nemo.ikkoku.de>
2013-07-07 23:34:51 +0200

    Boilerplate for message handling

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,6 +1,43 @@
 $(document).ready(function() {
+	function visitHandler(message) {
+	}
+
+	function joinHandler(message) {
+	}
+
+	function leaveHandler(message) {
+	}
+
+	function finishHandler(message) {
+	}
+
+	function gameOverHandler(message) {
+	}
+
+	function fatalStuffHandler(message) {
+	}
+
+	var messageHandler = {
+		0: visitHandler,
+		1: joinHandler,
+		2: leaveHandler,
+		3: finishHandler,
+		4: gameOverHandler,
+		5: fatalStuffHandler,
+	};
+
 	function getServerAddress() {
 		return location.host;
+	}
+
+	function parseMessage(rawMessage) {
+		var msg = JSON.parse(rawMessage);
+
+		return msg;
+	}
+
+	function handleMessage(message) {
+		messageHandler[message.Type](message)
 	}
 
 	var sock = new WebSocket("ws://"+getServerAddress()+"/client");
@@ -13,8 +50,8 @@ $(document).ready(function() {
 
 	sock.onmessage = function(m) {
 		console.log(m.data);
-		//var msg = jQuery.parseJSON(m.data);
 
+		handleMessage(parseMessage(m.data));
 		//$('#log').append('<li>' + msg + ' clicked on ' + msg.Message + '</li>');
 	}
 
