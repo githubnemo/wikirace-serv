ff5e2e6d7a7a9f38c90d0834f491ccb967de41d3
nemo <git@ningu.net>
2021-03-21 01:52:34 +0100

    Implement simple winner tests

--- game_test.go
+++ game_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestGameHasOwnerAsPlayer(t *testing.T) {
-	hostPlayerName := "the player"
+	hostPlayerName := "player 1"
 	gameWiki := &wikis.Wiki{}
 	game := NewGame(hostPlayerName, gameWiki, nil)
 
@@ -21,5 +21,75 @@ func TestGameHasOwnerAsPlayer(t *testing.T) {
 	if game.Players[0].Name != hostPlayerName {
 		t.Errorf("Expected first player to be %s but got %s",
 			hostPlayerName, game.Players[0].Name)
+	}
+}
+
+
+func simpleTwoPlayerGame() *Game {
+	hostPlayerName := "player 1"
+	gameWiki := &wikis.Wiki{}
+	game := NewGame(hostPlayerName, gameWiki, nil)
+
+	game.Start = "start page"
+	game.Goal = "goal page"
+
+	playerName1 := hostPlayerName
+	playerName2 := "player 2"
+
+	game.AddPlayer(playerName2)
+
+	player1 := game.GetPlayer(playerName1)
+	player2 := game.GetPlayer(playerName2)
+
+	player1.Visited(game.Start)
+	player2.Visited(game.Start)
+
+	return game
+}
+
+func TestSimpleGame(t *testing.T) {
+	game := simpleTwoPlayerGame()
+
+	if len(game.Players) != 2 {
+		t.Errorf("Unexpected number of players %d", len(game.Players))
+	}
+}
+
+func TestNobodyWonSimpleGameAtStart(t *testing.T) {
+	game := simpleTwoPlayerGame()
+
+	player1 := &game.Players[0]
+	player2 := &game.Players[1]
+
+	isWinner, isTempWinner := game.EvaluateWinner(player1)
+	if isWinner || isTempWinner {
+		t.Errorf("player1: winner: %t, temporary: %t, expected both false", isWinner, isTempWinner)
+	}
+
+	isWinner, isTempWinner = game.EvaluateWinner(player2)
+	if isWinner || isTempWinner {
+		t.Errorf("player2: winner: %t, temporary: %t, expected both false", isWinner, isTempWinner)
+	}
+}
+
+func TestWinnerSimple(t *testing.T) {
+	game := simpleTwoPlayerGame()
+
+	player1 := &game.Players[0]
+	player2 := &game.Players[1]
+
+	// player1 screws up, player2 finds the goal page
+	// there is no way player1 can catch up, player2 is the winner.
+	player1.Visited("other page")
+	player2.Visited(game.Goal)
+
+	isWinner, isTempWinner := game.EvaluateWinner(player1)
+	if isWinner || isTempWinner {
+		t.Errorf("player1: winner: %t, temporary: %t, expected both false", isWinner, isTempWinner)
+	}
+
+	isWinner, isTempWinner = game.EvaluateWinner(player2)
+	if !isWinner {
+		t.Errorf("player2: winner: %t, temporary: %t, expected both true", isWinner, isTempWinner)
 	}
 }
